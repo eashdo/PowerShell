cfe00d7256b201fdb18345f8d39eff5cfd5b5e44
Evan <90468946+eashdo@users.noreply.github.com>
2026-07-03 10:56:12 -0600

    MS Filtering changes

--- Exchange/Enable-AutoArchiving.ps1
+++ Exchange/Enable-AutoArchiving.ps1
@@ -4,21 +4,21 @@ Connect-exchangeonline
 and enable/apply those items. Adjust the filters as needed to match the conditions we are trying to target.
 #>
 
-#We can filter based on Archives, no need to store the entire user base for this portion.
-$NoArchives = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited -Filter 'UsageLocation -eq "Chile" -and ArchiveStatus -ne "Active"'
+#We cannot filter based on Archives/Policy anymore, thanks Microsoft for breaking that... 
+#We can filter by country, no need to store the entire user base.
+$Users = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited -Filter 'UsageLocation -eq "Bolivia" -or UsageLocation -eq "Uruguay"'
+
+$NoArchives = $Users | Where-Object {$_.ArchiveStatus -ne "Active"}
 Write-Host "There are currently" $NoArchives.count "user mailboxes in this batch with no archives."
 Write-Host "Correcting this issue..."
 
 Foreach ($N in $NoArchives){
 
     Enable-Mailbox $N.WindowsLiveID -Archive | Out-Null
-    Enable-Mailbox $N.WindowsLiveID -AutoExpandingArchive | Out-Null
 }
 
-#AutoExpandingArchiveEnabled and RetentionPolicy either don't permit filtering or don't filer properly. Thus we need to grab the entire userbase to action against, thanks MS...
-$Mailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox -ResultSize Unlimited -Filter 'UsageLocation -eq "Chile"' 
 
-$NoAutoExpanding =  $Mailboxes | Where-Object {$_.AutoExpandingArchiveEnabled -like "False"}
+$NoAutoExpanding =  $Users | Where-Object {$_.AutoExpandingArchiveEnabled -like "False"}
 Write-Host "There are currently" $NoAutoExpanding.count "user mailboxes in this batch without auto-expanding archives."
 Write-Host "Correcting this issue..."
 
@@ -27,7 +27,7 @@ Foreach ($NA in $NoAutoExpanding){
     Enable-Mailbox $NA.WindowsLiveID -AutoExpandingArchive | Out-Null
 }
 
-$NoMRMPolicy = $Mailboxes | Where-Object {$_.RetentionPolicy -eq "Default MRM Policy"}
+$NoMRMPolicy = $Users | Where-Object {$_.RetentionPolicy -eq "Default MRM Policy"}
 Write-Host "There are currently" $NoMRMPolicy.count "user mailboxes in this batch without a proper MRM Policy."
 Write-Host "Applying our 1-year policy to these mailboxes..."
 
@@ -37,6 +37,6 @@ Foreach ($NM in $NoMRMPolicy){
 }
 
 Write-host "All actions have been completed. Here is a summary:"
-Write-host $NoArchives.count "mailboxes had their archives enabled." (($NoArchives.count/$Mailboxes.count)*100)"%"
-Write-host $NoAutoExpanding.count "mailboxes had the auto-expanding archive enabled." (($NoAutoExpanding.count/$Mailboxes.count)*100)"%"
-Write-host $NoMRMPolicy.count "mailboxes had the 1-year auto-archiving policy assigned." (($NoMRMPolicy.count/$Mailboxes.count)*100)"%"
+Write-host $NoArchives.count "mailboxes had their archives enabled." (($NoArchives.count/$Users.count)*100)"%"
+Write-host $NoAutoExpanding.count "mailboxes had the auto-expanding archive enabled." (($NoAutoExpanding.count/$Users.count)*100)"%"
+Write-host $NoMRMPolicy.count "mailboxes had the 1-year auto-archiving policy assigned." (($NoMRMPolicy.count/$Users.count)*100)"%"
